96d050c372912bcca4775eaaff30421b14e92d34
Dolf Andringa <dolfandringa@gmail.com>
2018-10-11 13:18:14 +0800

    Added start to generate_views command to auto-generate views for a dataset

--- modama/cli.py
+++ modama/cli.py
@@ -10,7 +10,17 @@ from sqlalchemy.orm.exc import NoResultFound
 from modama import app, db, appbuilder
 from modama.models import common, dataset_base
 from modama.datasets import _datasets
+import modama.datasets
 from modama.data.psgc import barangays, municipalities, provinces, regions
+
+
+from inspect import isclass
+from flask_appbuilder import Model
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+from jinja2 import Template
+import re
+import autopep8
 
 
 app.config['CSRF_ENABLED'] = False
@@ -54,6 +64,68 @@ def create_admin(username=None, firstname=None, lastname=None,
         print('Admin User {0} created.'.format(username))
     else:
         print('No user created. An error occured!')
+
+
+def get_view_name(m):
+    return "{}View".format(m.__name__)
+
+
+def get_model_pretty(m, ds_title):
+    return re.sub("([a-z])([A-Z])", r"\1 \2", m.__name__.replace(ds_title, ''))
+
+
+@manager.command
+def generate_views(dataset):
+    ds = getattr(modama.datasets, dataset)
+    ds_title = dataset.title()
+    models = [m for m in ds.models.__dict__.values() if isclass(m)
+              and issubclass(m, Model)
+              and m.__module__ == 'modama.datasets.pawikan.models']
+    ##
+    template = Template("""
+    class {{model.__name__}}View:
+        _pretty_name = '{{model_pretty}}'
+        datamodel = GeoSQLAInterface({{model.__name__}})
+        add_columns = ["{{'", "'.join(add_columns)}}"]
+        list_columns = ["{{'", "'.join(list_columns)}}"]
+        edit_columns = ["{{'", "'.join(edit_columns)}}"]
+        show_columns = ["{{'", "'.join(show_columns)}}"]
+        related_views = [{{', '.join(related_views)}}]
+        add_title = ''
+        show_title = ''
+        list_title = ''
+        edit_title = ''
+        _conditional_relations = [
+            oneOf({}),
+        ]
+    """)
+    ##
+
+    for m in models[:-4]:
+        print(m.__name__)
+        interf = SQLAInterface(m)
+        props = [(k, v) for k, v in m.__dict__.items()
+                 if isinstance(v, InstrumentedAttribute)]
+        data = {'model': m,
+                'view_name': get_view_name(m),
+                'model_pretty': get_model_pretty(m, ds_title),
+                'add_columns': [],
+                'list_columns': [],
+                'show_columns': [],
+                'edit_columns': [],
+                'related_views': []
+                }
+        for name, prop in props:
+            if interf.is_relation(name):
+                rel_m = interf.get_related_model(name)
+                data['add_columns'].append(name)
+                data['related_views'].append(get_view_name(rel_m))
+            elif not interf.is_fk(name):
+                data['add_columns'].append(name)
+                data['edit_columns'].append(name)
+                data['show_columns'].append(name)
+                data['list_columns'].append(name)
+        print(autopep8.fix_code(template.render(data)))
 
 
 @manager.command
